3582945ca36db4936de067acaaac9d2506ae2323
Natalie Stollman <nataliestollman@wustl.edu>
2019-11-13 14:14:13 -0600

    bug fixed

--- BookUp/BookUp/AppDelegate.swift
+++ BookUp/BookUp/AppDelegate.swift
@@ -43,9 +43,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    func applicationDidFinishLaunching(_ application: UIApplication) {
-        <#code#>
-    }
+//    func applicationDidFinishLaunching(_ application: UIApplication) {
+//        <#code#>
+//    }
 
 }
 
