8e26025c9c8ec4ae4363a12dad16e6432cd36566
Lydia Ho <lydiaho@wustl.edu>
2019-11-28 15:00:33 -0600

    started history

--- BookUp/BookUp/History.swift
+++ BookUp/BookUp/History.swift
@@ -8,17 +8,59 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import Firebase
+import FirebaseFirestore
+import FirebaseCore
 
-class History: UIViewController {
+class History: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.recent.dequeueReusableCell(withIdentifier: "recentlyViewed", for: indexPath)
+        return cell
+    }
+    
+    var recents: [String] = []
+    var recentBooks: [Book] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        grabFirebaseData()
     }
     
     //books sold
     //books listed
     //contacted by
+
+    @IBOutlet weak var recent: UITableView!
     
+    var username = String((Auth.auth().currentUser?.email?.dropLast(10))!)
+    
+    func grabFirebaseData() {
+        Firestore.firestore().collection("/Users/").document(username).getDocument { (document, error) in
+            if let document = document, document.exists{
+                let data = document.data()
+                //                    .map(String.init(describing:)) ?? "nil"
+                do {
+                    let jsonData = try? JSONSerialization.data(withJSONObject: data!)
+                    let user = try JSONDecoder().decode(User.self, from: jsonData!)
+                    self.recents = user.RecentlyViewed
+                    self.grabBookData()
+                } catch {
+                    print ("something went wrong")
+                }
+            }
+        }
+    }
+    
+    func grabBookData() {
+        for r in recents {
+           print(r)
+        }
+    }
 }
 
